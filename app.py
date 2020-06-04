c812d2610458aebe47f013bd91535a41a7e75251
chaoyiwang <mithrandirwang1990@gmail.com>
2020-06-04 11:27:53 -0400

    Function: add list relations

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://chaoyiwang@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
@@ -14,11 +14,52 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
 	completed = db.Column(db.Boolean, nullable=False, default=False)
+	list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
 	def __repr__(self):
 		return f'<Todo {self.id} {self.description}>'
 
-#db.create_all()
+class TodoList(db.Model):
+	__tablename__ = 'todolists'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(), nullable=False)
+	todos = db.relationship('Todo', backref='list', lazy=True)
+
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+	try:
+		Todo.query.filter_by(id=todo_id).delete()
+		db.session.commit()
+	except:
+		db.session.rollback()
+	finally:
+		db.session.close()
+	return jsonify({ 'success': True })
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+	error = False
+	body = {}
+	try:
+		description = request.get_json()['description']
+		todo = Todo(description=description, completed=False)
+		db.session.add(todo)
+		db.session.commit()
+		body['id'] = todo.id
+		body['completed'] = todo.completed
+		body['description'] = todo.description
+	except:
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+	if error:
+		abort (400)
+	else:
+		return jsonify(body)
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
@@ -32,27 +73,6 @@ def set_completed_todo(todo_id):
 	finally:
 		db.session.close()
 	return redirect(url_for('index'))
-
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-	error = False
-	body = {}
-	try:
-		description = request.get_json()['description']
-		todo = Todo(description=description)
-		db.session.add(todo)
-		db.session.commit()
-		body['description'] = todo.description
-	except:
-		error = True
-		db.session.rollback()
-		print(sys.exc_info())
-	finally:
-		db.session.close()
-	if error:
-		abort (400)
-	else:
-		return jsonify(body)
 
 @app.route('/')
 def index():
